7fc11f65066ecc2e807ac0dddd45b1fbd13d631e
Fabian Yate <fabianyate31@gmail.com>
2024-05-10 22:30:38 -0500

    add authentication filter to the api gateway

---
+++ cloud-config/spring-cloud-gateway/src/main/java/com/pos/springcloudgateway/filter/AuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.pos.springcloudgateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+    private final RouteValidator routeValidator;
+    private final RestTemplate restTemplate;
+
+    public AuthenticationFilter(RouteValidator routeValidator, RestTemplate restTemplate) {
+        super(Config.class);
+        this.routeValidator = routeValidator;
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (routeValidator.isSecured.test(exchange.getRequest())) {
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("missing authorization header");
+                }
+
+                String authHeader = Objects.requireNonNull(exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
+
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                try {
+                    restTemplate.getForObject("http://IDENTITY-SERVICE//validate?token" + authHeader, String.class);
+                } catch (Exception e) {
+                    System.out.println("invalid access...!");
+                    throw new RuntimeException("un authorized access to application");
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+
+    }
+}
